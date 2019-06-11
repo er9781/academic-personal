6635d81a5e2134c863b9215a921fa2a41fd292f5
Simon Labute <labute.simon@gmail.com>
2019-06-11 01:30:26 -0700

    change 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -6,8 +6,8 @@ import SEO from "../components/seo";
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>I'm not a professional coder.</h1>
+    <p>Something should be here, but it's not. I'll maybe fix it one day.</p>
   </Layout>
 );
 
